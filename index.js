45ff1b4d2099a746b6c4acdb71c534f5b5964f19
Ang Li <laauron@gmail.com>
2017-08-04 06:47:12 +0000

    Initial commit of a TODO app

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
+const pug = require('pug')
+const bodyParser = require('body-parser')
+
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'))
+
+// Store TODOs in memory
+var todos = [];
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('index', {todos: todos});
+});
+
+app.post('/add', function (req, res) {
+  var newTodo = req.body.todo;
+  console.log("new TODO item:", newTodo);
+  todos.push(newTodo);
+  res.redirect(302, '/');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
